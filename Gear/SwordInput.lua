e8a7b88ff3f67179175862ddb43ddac070319b8a
AxonMega <axonmega@gmail.com>
2017-05-28 09:12:12 -0400

    Create SwordInput.lua

---
+++ Gear/SwordInput.lua
@@ -0,0 +1,37 @@
+--scripted by AxonMega
+
+local sword = script.Parent.Parent
+local rFunc = sword.SwordScript:WaitForChild("Function")
+local mouse = game.Players.LocalPlayer:GetMouse()
+local equipped = false
+local striking = false
+local canStrike = false
+
+local function isGood()
+	return sword.Enabled and equipped and canStrike and not striking
+end
+
+local function onButton1Down()
+	if isGood() then
+		striking = true
+		rFunc:InvokeServer()
+		striking = false
+	end
+end
+
+local function onEquipped()
+	equipped = true
+	mouse.Icon = "rbxasset://textures\\gunCursor.png"
+	wait(0.2)
+	canStrike = true
+end
+
+local function onUnequipped()
+	equipped = false
+	canStrike = false
+	mouse.Icon = ""
+end
+
+mouse.Button1Down:Connect(onButton1Down)
+sword.Equipped:Connect(onEquipped)
+sword.Unequipped:Connect(onUnequipped)
