7baf872c019f7bd6eaed79eadff3d44350f4a10b
AxonMega <axonmega@gmail.com>
2017-05-28 09:18:32 -0400

    Update Gameplay.lua

--- Misc/Gameplay.lua
+++ Misc/Gameplay.lua
@@ -1,35 +1,50 @@
 --scripted by AxonMega
 
---DEFINE VARIABLES
+--DECLARE VARIABLES
 
 local players = game:WaitForChild("Players")
-local playerDiedC = workspace:WaitForChild("-PlayerDiedC-")
+local killfeedEvent = workspace:WaitForChild("-KillfeedEvent-")
 local storage = game:WaitForChild("ServerStorage")
 local modFolder = game.ServerScriptService:WaitForChild("GameplayModuleScripts")
 local adminUtils = require(modFolder:WaitForChild("AdminCommands"))
 local shipManager = require(modFolder:WaitForChild("ShipManager"))
-local gun = storage:WaitForChild("Gear"):WaitForChild("Wasp-P2")
-local gunScript = storage:WaitForChild("Scripts"):WaitForChild("GunScript")
+local setupOutpost = require(modFolder:WaitForChild("Outposts"))
+local gear = storage:WaitForChild("Gear")
+local scripts = storage:WaitForChild("Scripts")
+local gun = gear:WaitForChild("Wasp-P2")
 local debris = game:GetService("Debris")
 local passcodeDied = "secret"
-local size = Vector3.new(1000, 20, 1000)
 local ms = game:GetService("MarketplaceService")
 local stats = {}
 local latestKillfeed = {}
 local gamePasses = {
-	[613029662] = {storage.Gear:WaitForChild("Ronin-X4"), storage.Scripts:WaitForChild("SwordScript")},
-	[628589421] = {storage.Gear:WaitForChild("Boa-S5"), gunScript}
+	[613029662] = {gear:WaitForChild("Ronin-X4"), scripts:WaitForChild("SwordScript")},
+	[628589421] = {gear:WaitForChild("Boa-S5"), scripts:WaitForChild("GunScript")},
+	[669635802] = {gear:WaitForChild("Falcon-J6"), scripts:WaitForChild("JetpackScript")},
+	[740135495] = {gear:WaitForChild("K3v1n-B7 Spawner"), scripts:WaitForChild("RobotSpawnerScript")}
 }
+
+--HANDLE GAME PASSES
+
+local function onGiveGearRequest(player, assetId)
+	local asset = gamePasses[assetId]
+	if asset then
+		local toolClone = asset[1]:Clone()
+		asset[2]:Clone().Parent = toolClone
+		toolClone.Parent = player.Backpack
+	end
+end
 
 --HANDLE BATTLE STATS
 
 local function onPlayerDied(victor, victim, passcode)
 	if passcode == passcodeDied then
-		playerDiedC:FireAllClients(victor, victim, passcodeDied)
-		table.insert(latestKillfeed, 1, {victor.Name .. " Killed " .. victim.Name, victor.TeamColor.Color})
-		latestKillfeed[7] = nil
 		stats[victor].kills = stats[victor].kills + 1
 		victor.leaderstats.Kills.Value = stats[victor].kills
+		print(victor, "has killed", victim) --TESTING
+		killfeedEvent:FireAllClients(victor, victim, passcodeDied)
+		table.insert(latestKillfeed, 1, {victor.Name .. " Killed " .. victim.Name, victor.TeamColor.Color})
+		latestKillfeed[7] = nil
 	end
 end
 
@@ -51,8 +66,11 @@ local function onPlayerAdded(player)
 	local function onCharacterAdded(character)
 		local gunClone = gun:Clone()
 		gunClone.Parent = player.Backpack
-		gunScript:Clone().Parent = gunClone
+		scripts.GunScript:Clone().Parent = gunClone
+		local dead = false
 		local function onDied()
+			if dead then return end
+			dead = true
 			stats[player].deaths = stats[player].deaths + 1
 			player.leaderstats.Deaths.Value = stats[player].deaths
 		end
@@ -60,8 +78,8 @@ local function onPlayerAdded(player)
 		for id, asset in pairs(gamePasses) do
 			if isAdmin or ms:PlayerOwnsAsset(player, id) then
 				local toolClone = asset[1]:Clone()
-				toolClone.Parent = player.Backpack
 				asset[2]:Clone().Parent = toolClone
+				toolClone.Parent = player.Backpack
 			end
 		end
 	end
@@ -88,26 +106,15 @@ end
 
 players.PlayerAdded:Connect(onPlayerAdded)
 players.PlayerRemoving:Connect(onPlayerRemoving)
-playerDiedC.OnServerEvent:Connect(onPlayerDied)
+workspace:WaitForChild("-PlayerDied-").Event:Connect(onPlayerDied)
 workspace:WaitForChild("-GetKillfeed-").OnServerInvoke = onKillfeedRequest
-workspace:WaitForChild("-PlayerDiedS-").Event:Connect(onPlayerDied)
+workspace:WaitForChild("-GivePlayerGear-").Event:Connect(onGiveGearRequest)
 
---SETUP SHIP SPAWNERS
+--SETUP SHIP SPAWNERS AND OUTPOSTS
 
 shipManager:setupSpawners(workspace:WaitForChild("RedBase"):WaitForChild("ShipSpawners"))
 shipManager:setupSpawners(workspace:WaitForChild("BlueBase"):WaitForChild("ShipSpawners"))
-
---GENERATE TERRAIN
-
-while not workspace.Terrain do wait() end
-local terrain = workspace.Terrain
-
-local function fill(pos)
-	local region = Region3.new(pos - size, pos + size)
-	terrain:FillRegion(region:ExpandToGrid(4), 4, Enum.Material.Water)
-end
-
-fill(Vector3.new(1000, 30, 1000))
-fill(Vector3.new(1000, 30, -1000))
-fill(Vector3.new(-1000, 30, 1000))
-fill(Vector3.new(-1000, 30, -1000))
+--[[setupOutpost(workspace:WaitForChild("OutpostA"))
+setupOutpost(workspace:WaitForChild("OutpostB"))
+setupOutpost(workspace:WaitForChild("OutpostX"))
+setupOutpost(workspace:WaitForChild("OutpostY"))]]
